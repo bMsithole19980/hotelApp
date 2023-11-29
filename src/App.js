6979d8ee1594f57c46d2a7eb3d2cbc8719e5af8f
bMsithole19980 <bmsithole19998@gmail.com>
2023-11-29 14:28:37 +0200

    header ui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './App.css';
 import { BrowserRouter  , Routes ,Route  } from 'react-router-dom';
@@ -8,27 +9,25 @@ import Register from './components.js/register';
 import ForgortPassword from './components.js/forgortPassword';
 import Reserve from './components.js/reserve';
 import SuccessReserved from './components.js/successReserved';
-import AdminScreen from './components.js/AdminScreen';
-import ReservationDetails from './components.js/ReservationDetails';
+import ReservationDetails from './components.js/ReservationDetails'
 function App() {
   return (
     
   
       <BrowserRouter>
           
-          <Routes >
-          
-            <Route path='/' element={<Login/>} />
+          <Routes>
+            
+            
+            <Route path='/' element={<Login />} />
             <Route path='/header' element={<Header/>} />
             <Route path='/register' element={<Register />} />
             <Route path='/forgortPassword' element={<ForgortPassword />} />
             <Route path='/roomReserv' element={<RoomReserv/>}/>
             <Route path='/successReserved' element={<SuccessReserved/>}></Route>
             <Route path='/reserve' element={<Reserve/>} /> 
-            <Route path='/reserveDetails' element={<ReservationDetails/>} />
-            <Route path='/adminScreen' element={<AdminScreen/>} />  
+            <Route path='/reserveDetails' element={<ReservationDetails/>} /> 
 
-          
           </Routes>
         </BrowserRouter>
 
